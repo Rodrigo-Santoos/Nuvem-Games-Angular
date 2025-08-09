9004d9923d067fe0aa6f93329fe4a22869339b33
Rodrigo-Santoos <rodrigoyukimide@gmail.com>
2025-08-09 17:03:45 -0300

    ajustando para colocar o PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -20,6 +20,7 @@ import { PaginaPrincipalComponent } from './pagina-principal/pagina-principal.co
 import {MatSidenavModule} from '@angular/material/sidenav';
 import { MenuLateralComponent } from './menu-lateral/menu-lateral.component';
 import { FormsModule } from '@angular/forms';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 
 
@@ -45,7 +46,13 @@ import { FormsModule } from '@angular/forms';
     MatFormFieldModule,
     MatInputModule,
     MatSidenavModule,
-    FormsModule
+    FormsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [
     provideClientHydration(withEventReplay()),
